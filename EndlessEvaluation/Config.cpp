a8291d54ba6b281b221dece5ec7fb99196286022
Romez1990 <Romez1990@yandex.ru>
2018-12-29 09:50:59 +0300

    Create Config.hpp and Config.cpp with empty functions

---
+++ EndlessEvaluation/Config.cpp
@@ -0,0 +1,31 @@
+#include "Config.hpp"
+
+void config_warning(const char* path, const bool is_file)
+{
+	
+}
+
+void config_error(const char* path, const bool is_file)
+{
+	
+}
+
+const char* get_homedir()
+{
+	return nullptr;
+}
+
+const char* c_str_concat(const char* c1, const char* c2)
+{
+	return nullptr;
+}
+
+void clean_dir(const char* dir)
+{
+	
+}
+
+void edit_config(const char** programs, const int& program_number)
+{
+
+}
---
+++ EndlessEvaluation/Config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+void config_warning(const char* path, const bool is_file);
+void config_error(const char* path, const bool is_file);
+const char* get_homedir();
+const char* c_str_concat(const char* c1, const char* c2);
+void clean_dir(const char* dir);
+void edit_config(const char** programs, const int& program_number);
